5906b785002711a7ce51b9f1d2f6081663e0af04
Felipe Campara <fcamparasilva@gmail.com>
2019-09-19 08:28:30 -0400

    Feat (Web Hooks): Learn hooks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
-import React from 'react'
+import React, { useState, useEffect, useMemo, useCallback } from 'react'
 
 function App () {
+  // Create States
+  const [techs, setTechs] = useState(['ReactJS', 'React Native'])
+  const [newTech, setNewTech] = useState('')
+
+  // Save memory on handle, only handle one time. Only recreated on memory when change newTech or techs
+  const handleAdd = useCallback(() => {
+    if (!newTech) return false
+    setTechs([...techs, newTech])
+    setNewTech('')
+  }, [newTech, techs])
+
+  // ComponentMount
+  useEffect(() => {
+    const storageTechs = localStorage.getItem('techs')
+
+    if (storageTechs) setTechs(JSON.parse(storageTechs))
+
+    // ComponentUnmount
+    return () => {}
+  }, [])
+
+  // Watch variable
+  useEffect(() => {
+    localStorage.setItem('techs', JSON.stringify(techs))
+  }, [techs])
+
+  // Save memory when render pages
+  const techSize = useMemo(() => techs.length, [techs])
+
   return (
-    <div className='App'>
-      <h1>Hello Word</h1>
-    </div>
+    <>
+      <ul>
+        {techs.map(tech => (
+          <li key={tech}>{tech}</li>
+        ))}
+      </ul>
+      <strong>Você tem {techSize} tecnologias</strong>
+      <br />
+      <input value={newTech} onChange={e => setNewTech(e.target.value)} />
+      <button type='button' onClick={handleAdd}>
+      Adicionar
+      </button>
+    </>
   )
 }
 
